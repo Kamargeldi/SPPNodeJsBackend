11c38220d538cc2008cb64f8f84a6dcbfebb683f
Kamar <kmprojects1998@gmail.com>
2021-02-03 18:12:23 +0300

    Add jsonwebtoken module.

--- webapp/Controllers/LoginController.js
+++ webapp/Controllers/LoginController.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const tokenKey = '1a2b-3c4d-5e6f-7g8h';
 
 exports.loginGet = function(request, response)
 {
@@ -8,5 +11,5 @@ exports.loginGet = function(request, response)
 
 exports.loginPost = function(request, response)
 {
-    response.status(200).send("Login post action.");
+    
 };
--- webapp/Controllers/RegisterController.js
+++ webapp/Controllers/RegisterController.js
@@ -10,5 +10,13 @@ exports.registerGet = function(request, response)
 
 exports.registerPost = function(request, response)
 {
-    response.status(200).send("Register post action.");    
+    var email = request.body.email;
+    var password = request.body.password;
+    dal.connection.query("insert into user(`email`, `password`) values(?, ?)", [email, password])
+    .then((result) => {
+        response.status(201).json("Register success.");
+    }).catch((err) => {
+        console.log("Error: " + err.message);
+        response.status(409).json({message : "User already exists."});
+    });
 };
